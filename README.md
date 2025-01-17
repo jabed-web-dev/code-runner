ae6baebfb6cd9f2bbf17d37e61341a3839dc8129
Jabed Hossain <jabedmd837@gmail.com>
2025-01-17 23:59:21 +0600

    Initial commit for new branch

---
+++ README.md
@@ -0,0 +1,26 @@
+# code-runner
+
+build:\
+  $ `cargo build --release`
+
+uses:\
+  $ `run --help`\
+  Usage: --pause? --clear? \<program> \<script> [arguments...]
+
+test:\
+  $ `run --pause node test/main.js arg1 arg2`\
+  \# When run build exe from source like C,C++,Rust use with arg '-'\
+  $ `run main.exe -`
+
+  \# If your have args remove this '-'\
+  $ `run main.exe arg1 arg2`
+
+> **Access globally move run.exe to the env folder path/bin**\
+> $ `run node main.js`
+> 
+> **Run with vscode code runner:**
+```json
+"code-runner.executorMap": {
+    "javascript": "run --clear node $fullFileName"
+}
+```
---
+++ test/main.js
@@ -0,0 +1,11 @@
+import process from 'node:process';
+console.log(process.argv.slice(2));
+
+console.log('\x1b[30m0 Hello World!\x1b[0m \x1b[90m Hello World!\x1b[0m')
+console.log('\x1b[31m1 Hello World!\x1b[0m \x1b[91m Hello World!\x1b[0m')
+console.log('\x1b[32m2 Hello World!\x1b[0m \x1b[92m Hello World!\x1b[0m')
+console.log('\x1b[33m3 Hello World!\x1b[0m \x1b[93m Hello World!\x1b[0m')
+console.log('\x1b[34m4 Hello World!\x1b[0m \x1b[94m Hello World!\x1b[0m')
+console.log('\x1b[35m5 Hello World!\x1b[0m \x1b[95m Hello World!\x1b[0m')
+console.log('\x1b[36m6 Hello World!\x1b[0m \x1b[96m Hello World!\x1b[0m')
+console.log('\x1b[37m7 Hello World!\x1b[0m \x1b[97m Hello World!\x1b[0m')
